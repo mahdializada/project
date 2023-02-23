894a6ca18e338e2ec303bf265d0e9258d74a55d5
mahdializada <mahdializada761@gmail.com>
2023-02-23 13:12:44 +0430

    Create 2022_10_23_054923_create_bank_accounts_table.php

---
+++ backend/database/factories/Advertisement/2022_10_23_054923_create_bank_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('card_number');
+            $table->string('owner');
+            $table->string('bank_name');
+            $table->foreignUuid('user_id')->constrained('users')->cascadeOnUpdate();
+            $table->foreignUuid('add_account_id')->constrained('ad_accounts')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+}
