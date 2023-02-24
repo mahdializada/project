9f6b6efa1292ecb3b55c7d106b365c5916dc6dbb
mahdializada <mahdializada761@gmail.com>
2023-02-24 14:14:47 +0430

    Create Header.vue

---
+++ frontend/components/assign/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-card style="height: 100px">
+      <v-row>
+        <v-col cols="1">
+          <div class="d-flex align-center">
+            <v-avatar
+              size="70"
+              style="
+                background-color: lightblue;
+                margin-left: 20px;
+                margin-top: 5px;
+              "
+            >
+              <i
+                class="fa fa-building"
+                style="font-size: 20px; color: black"
+              ></i>
+            </v-avatar>
+          </div>
+        </v-col>
+        <v-col cols="5">
+          <div class="d-flex">
+            <span>
+              <h2 class="ml-4 mt-5" style="height: 20px">Scholarships</h2>
+            </span>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
