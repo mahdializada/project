9982df0ec62f3d86354c21e1327fbb2c6880e806
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-03-03 15:40:59 +0430

    dd

---
+++ frontend/pages/auth/reset-password.vue
@@ -0,0 +1,454 @@
+<template>
+  <client-only>
+    <div>
+      <!-- The video -->
+      <video autoplay muted loop id="myVideo">
+        <source src="/videos/bg-login-video.mp4" type="video/mp4" />
+      </video>
+      <div style="position: absolute; left: 2%; top: 30px">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="176.229"
+          height="39.61"
+          viewBox="0 0 176.229 39.61"
+        >
+          <defs>
+            <linearGradient
+              id="linear-gradient"
+              x1="-0.521"
+              y1="1.391"
+              x2="1.035"
+              y2="0.234"
+              gradientUnits="objectBoundingBox"
+            >
+              <stop offset="0.52" stop-color="#f16933" />
+              <stop offset="0.845" stop-color="#faae36" />
+              <stop offset="1" stop-color="#ffd134" />
+            </linearGradient>
+          </defs>
+          <g
+            id="Teeb_al_hoor"
+            data-name="Teeb al hoor"
+            transform="translate(-44.561 -18.9)"
+          >
+            <g
+              id="Group_1"
+              data-name="Group 1"
+              transform="translate(44.561 18.9)"
+            >
+              <path
+                id="Path_1"
+                data-name="Path 1"
+                d="M72.244,47.874c1.365.787,2.729,1.575,4.042,2.415,2.1,1.207,4.2,2.467,6.246,3.674a4.66,4.66,0,0,1,1.785,1.417c1.155,1.837-1.207,3.832-2.939,2.887-1.312-.682-2.52-1.522-3.779-2.257-2.1-1.26-4.2-2.467-6.351-3.727a13.679,13.679,0,0,1-1.47-.892,2,2,0,0,1-.472-.682,8.118,8.118,0,0,1-.157-.892l-1-.63a21.587,21.587,0,0,1-4.042,3.674,13.367,13.367,0,0,1-10.446,1.1,13.959,13.959,0,0,1-6.719-4.777,12.729,12.729,0,0,1-1.732-3.359,12.915,12.915,0,0,1-.472-2.047,4.123,4.123,0,0,1,.052-2.52,2.1,2.1,0,0,1,1.785-1.05,2.043,2.043,0,0,1,2.152,1.732c.157.945.84,7.821,8.871,8.346,4.934-.315,8.556-3.149,8.871-8.923-.052-2.257-1.155-8.5-9.028-8.923-3.307.157-5.511,1.522-8.346,5.092a2.1,2.1,0,0,1-2.992.315,1.6,1.6,0,0,1-.63-1.155c-.1-1.207,1-2.572,1.68-3.464a11.646,11.646,0,0,1,5.092-3.884c4.094-1.47,5.669-1.207,8.031-.84A26.339,26.339,0,0,0,57.6,25.776c-.682-.63-1.68-1.1-1.995-1.995a2.043,2.043,0,0,1,.1-1.47,2.369,2.369,0,0,1,1.1-1.05,2.247,2.247,0,0,1,2.1.577l1.89,1.575a29.265,29.265,0,0,1,6.614,8.766c-.052-.052,0-.21,0-.262a3.1,3.1,0,0,0-.1-.735,24.1,24.1,0,0,0-.525-2.677,24.905,24.905,0,0,0-1.365-3.937,20.478,20.478,0,0,0-1-1.837,4.118,4.118,0,0,1-.787-1.68v-.157a1.911,1.911,0,0,1,1.89-1.995c1.417.052,2.2,1.68,2.31,1.995a30.093,30.093,0,0,1,2.939,7.4s.42,2.1.472,2.415a34.024,34.024,0,0,0,1.05-7.139,1.6,1.6,0,0,1,1.942-1.47c1.207.052,1.837.945,1.89,2.257.1,2.2-2.047,14.592-6.876,22.833l.892.63C70.3,47.979,71.142,47.192,72.244,47.874Z"
+                transform="translate(-44.561 -18.9)"
+                fill="url(#linear-gradient)"
+              />
+              <path
+                id="Path_2"
+                data-name="Path 2"
+                d="M63.411,62.012a1.523,1.523,0,0,1-1.522-1.522,1.471,1.471,0,0,1,1.155-1.47,3.473,3.473,0,1,0,1.785,3.044,4.052,4.052,0,0,0-.1-.787A1.545,1.545,0,0,1,63.411,62.012Zm-1.1,1.785a.63.63,0,1,1,.63-.63A.676.676,0,0,1,62.309,63.8Z"
+                transform="translate(-50.899 -37.761)"
+              />
+              <path
+                id="Path_3"
+                data-name="Path 3"
+                d="M65.4,60.87a1.523,1.523,0,0,0,1.522,1.522,1.483,1.483,0,0,0,1.312-.787A3.351,3.351,0,0,0,66.5,59.4,1.541,1.541,0,0,0,65.4,60.87Z"
+                transform="translate(-54.462 -38.142)"
+                fill="#fff"
+              />
+              <path
+                id="Path_4"
+                data-name="Path 4"
+                d="M65.73,66.2a.63.63,0,1,0,.63.63A.676.676,0,0,0,65.73,66.2Z"
+                transform="translate(-54.319 -41.372)"
+                fill="#fff"
+              />
+            </g>
+            <g
+              id="Group_4"
+              data-name="Group 4"
+              transform="translate(95.077 24.097)"
+            >
+              <g id="Group_2" data-name="Group 2" transform="translate(0.787)">
+                <path
+                  id="Path_5"
+                  data-name="Path 5"
+                  d="M156.315,43.362h-8.241L146.5,47.142h-4.2L150.226,29.4h4.042l7.926,17.742h-4.3Zm-1.26-3.149-2.834-6.771-2.782,6.771Z"
+                  transform="translate(-142.3 -29.085)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_6"
+                  data-name="Path 6"
+                  d="M183.7,29.4h4.094V43.782h8.871v3.359H183.7Z"
+                  transform="translate(-161.969 -29.085)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_7"
+                  data-name="Path 7"
+                  d="M242.819,29.4V47.142h-4.094V39.9h-8.031v7.244H226.6V29.4h4.094v6.981h8.031V29.4Z"
+                  transform="translate(-182.351 -29.085)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_8"
+                  data-name="Path 8"
+                  d="M263.3,37.986c0-5.249,4.094-9.186,9.711-9.186,5.564,0,9.711,3.884,9.711,9.186s-4.147,9.186-9.711,9.186S263.3,43.287,263.3,37.986Zm15.275,0a5.565,5.565,0,1,0-11.128,0,5.565,5.565,0,1,0,11.128,0Z"
+                  transform="translate(-199.787 -28.8)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_9"
+                  data-name="Path 9"
+                  d="M304,37.986c0-5.249,4.094-9.186,9.711-9.186,5.564,0,9.711,3.884,9.711,9.186s-4.147,9.186-9.711,9.186S304,43.287,304,37.986Zm15.275,0a5.539,5.539,0,1,0-11.075,0,5.539,5.539,0,1,0,11.075,0Z"
+                  transform="translate(-219.124 -28.8)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_10"
+                  data-name="Path 10"
+                  d="M358.185,47.142l-3.412-4.934h-3.779v4.934H346.9V29.4h7.664c4.724,0,7.716,2.467,7.716,6.456a5.828,5.828,0,0,1-3.622,5.616l3.989,5.721h-4.462Zm-3.832-14.382h-3.359v6.194h3.359c2.52,0,3.779-1.155,3.779-3.1C358.08,33.914,356.821,32.759,354.354,32.759Z"
+                  transform="translate(-239.505 -29.085)"
+                  fill="#fff"
+                />
+              </g>
+              <g
+                id="Group_3"
+                data-name="Group 3"
+                transform="translate(0 23.253)"
+              >
+                <path
+                  id="Path_11"
+                  data-name="Path 11"
+                  d="M140.8,76.092a3.12,3.12,0,0,1,5.3-2.1l-.525.525a2.183,2.183,0,0,0-1.627-.682,2.258,2.258,0,1,0,0,4.514,2.1,2.1,0,0,0,1.627-.682l.525.525a3.081,3.081,0,0,1-5.3-2.1Z"
+                  transform="translate(-140.8 -73.1)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_12"
+                  data-name="Path 12"
+                  d="M155.3,76.092a3.1,3.1,0,1,1,3.1,2.992A2.952,2.952,0,0,1,155.3,76.092Zm5.354,0a2.178,2.178,0,0,0-2.257-2.257,2.221,2.221,0,0,0-2.31,2.257,2.283,2.283,0,0,0,4.567,0Z"
+                  transform="translate(-147.689 -73.1)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_13"
+                  data-name="Path 13"
+                  d="M178.254,79.079V74.827l-2.1,3.569h-.367l-2.1-3.517v4.252H172.9V73.2h.682l2.467,4.094,2.415-4.094h.682v5.879h-.892Z"
+                  transform="translate(-156.051 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_14"
+                  data-name="Path 14"
+                  d="M196.654,79.079V74.827l-2.1,3.569h-.367l-2.1-3.517v4.252H191.3V73.2h.682l2.467,4.094,2.415-4.094h.682v5.879h-.892Z"
+                  transform="translate(-164.793 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_15"
+                  data-name="Path 15"
+                  d="M213.852,78.344v.735H209.6V73.2h4.147v.735H210.44V75.72h2.939v.735H210.44v1.89Z"
+                  transform="translate(-173.487 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_16"
+                  data-name="Path 16"
+                  d="M227.337,79.079l-1.26-1.785H224.24v1.785h-.84V73.2h2.31c1.522,0,2.467.787,2.467,2.047a1.816,1.816,0,0,1-1.312,1.837l1.365,1.942h-.892Zm0-3.832c0-.84-.577-1.312-1.627-1.312h-1.47v2.624h1.417C226.759,76.559,227.337,76.087,227.337,75.247Z"
+                  transform="translate(-180.043 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_17"
+                  data-name="Path 17"
+                  d="M237.3,76.092a3.12,3.12,0,0,1,5.3-2.1l-.525.525a2.183,2.183,0,0,0-1.627-.682,2.258,2.258,0,1,0,0,4.514,2.1,2.1,0,0,0,1.627-.682l.525.525a3.081,3.081,0,0,1-5.3-2.1Z"
+                  transform="translate(-186.647 -73.1)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_18"
+                  data-name="Path 18"
+                  d="M252.9,73.2h.84v5.879h-.84Z"
+                  transform="translate(-194.059 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_19"
+                  data-name="Path 19"
+                  d="M264.067,77.609h-3.1l-.63,1.47h-.84l2.677-5.879h.84l2.677,5.879H264.8Zm-.315-.682-1.26-2.887-1.26,2.887Z"
+                  transform="translate(-197.194 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_20"
+                  data-name="Path 20"
+                  d="M275.9,73.2h.84v5.144h3.149v.735H275.9Z"
+                  transform="translate(-204.986 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_21"
+                  data-name="Path 21"
+                  d="M301.039,77.5c0,1-.735,1.575-2.257,1.575H296V73.2h2.625c1.365,0,2.1.577,2.1,1.522a1.37,1.37,0,0,1-.787,1.312A1.428,1.428,0,0,1,301.039,77.5Zm-4.2-3.622v1.89h1.732c.84,0,1.365-.315,1.365-.945s-.472-.945-1.365-.945Zm3.359,3.569c0-.682-.525-1-1.417-1H296.84V78.4h1.942Q300.2,78.4,300.2,77.452Z"
+                  transform="translate(-214.536 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_22"
+                  data-name="Path 22"
+                  d="M315.237,79.079l-1.26-1.785H312.14v1.785h-.84V73.2h2.31c1.522,0,2.467.787,2.467,2.047a1.816,1.816,0,0,1-1.312,1.837l1.365,1.942h-.892Zm-.052-3.832c0-.84-.577-1.312-1.627-1.312H312.14v2.624h1.417C314.659,76.559,315.184,76.087,315.184,75.247Z"
+                  transform="translate(-221.805 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_23"
+                  data-name="Path 23"
+                  d="M325.2,76.092a3.1,3.1,0,1,1,3.1,2.992A2.952,2.952,0,0,1,325.2,76.092Zm5.354,0a2.178,2.178,0,0,0-2.257-2.257,2.221,2.221,0,0,0-2.31,2.257,2.283,2.283,0,0,0,4.567,0Z"
+                  transform="translate(-228.408 -73.1)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_24"
+                  data-name="Path 24"
+                  d="M344.59,76.454l-1.05,1.1v1.522h-.84V73.2h.84v3.254l3.2-3.254h.945l-2.52,2.624,2.677,3.2h-1Z"
+                  transform="translate(-236.723 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_25"
+                  data-name="Path 25"
+                  d="M361.552,78.344v.735H357.3V73.2h4.147v.735H358.14V75.72h2.939v.735H358.14v1.89Z"
+                  transform="translate(-243.659 -73.148)"
+                  fill="#fff"
+                />
+                <path
+                  id="Path_26"
+                  data-name="Path 26"
+                  d="M375.037,79.079l-1.26-1.785H371.94v1.785h-.84V73.2h2.31c1.522,0,2.467.787,2.467,2.047a1.816,1.816,0,0,1-1.312,1.837l1.365,1.942h-.892Zm0-3.832c0-.84-.577-1.312-1.627-1.312h-1.417v2.624h1.417C374.459,76.559,375.037,76.087,375.037,75.247Z"
+                  transform="translate(-250.216 -73.148)"
+                  fill="#fff"
+                />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+
+      <v-card class="pa-2">
+        <v-card-title class="justify-center display-1 mb-2"
+          >{{ $tr("reset_title") }}
+        </v-card-title>
+        <div class="overline">{{ status }}</div>
+        <div class="error--text mt-2 mb-4">{{ error }}</div>
+
+        <TextField
+          label="password"
+          :type="showPass ? 'text' : 'password'"
+          :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPass = !showPass"
+          v-model.trim="$v.newPassword.password.$model"
+          :rules="Rules.passwordRule($v.newPassword, $root)"
+          :invalid="$v.newPassword.password.$invalid"
+        />
+        <TextField
+          label="confirm_password"
+          :type="showPass2 ? 'text' : 'password'"
+          :append-icon="showPass2 ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPass2 = !showPass2"
+          v-model.trim="$v.newPassword.confirm_password.$model"
+          :rules="Rules.password2Rule($v.newPassword, $root)"
+          :invalid="$v.newPassword.confirm_password.$invalid"
+        />
+        <div class="text-left mb-2">
+          <router-link :to="'/auth/signin'">
+            {{ $tr("back_to_sign_in") }}
+          </router-link>
+        </div>
+        <v-btn
+          :loading="isLoading"
+          :disabled="isLoading"
+          block
+          depressed
+          x-large
+          color="primary"
+          @click="confirmPasswordReset"
+          >{{ $tr("reset_password") }}
+        </v-btn>
+      </v-card>
+    </div>
+  </client-only>
+</template>
+<script>
+/*
+|---------------------------------------------------------------------
+| Reset Page Component
+|---------------------------------------------------------------------
+|
+| Page Form to insert new password and proceed to sign in
+|
+*/
+import TextField from "../../components/common/TextField";
+import Validations from "~/validations/validations";
+import Rules from "~/validations/rules";
+
+export default {
+  meta: {
+    hasAuth: false,
+  },
+  auth: false,
+  layout: "auth",
+  components: { TextField },
+  async asyncData({ query, error, $axios }) {
+    if (query && query.email && query.token) {
+      const token = query.token;
+      const email = query.email;
+      try {
+        const url = `reset-password?has-reset-token=true&token=${token}&email=${email}`;
+        const response = await $axios.post(url);
+        if (response.status === 200 && response.data.result) {
+          return { token, email };
+        }
+        error({
+          statusCode: response.status,
+          message: this.$tr("invalid_email_or_token"),
+        });
+      } catch (exception) {
+        error({
+          statusCode: exception.response.status,
+          message: this.$tr("invalid_email_or_token"),
+        });
+      }
+    }
+    error({
+      statusCode: "401",
+      message: this.$tr("unauthorized_to_access_this_page"),
+    });
+  },
+
+  data() {
+    return {
+      Rules: Rules,
+      isLoading: false,
+      showPass: false,
+      showPass2: false,
+      showNewPassword: true,
+      newPassword: {
+        password: "",
+        confirm_password: "",
+        email: "",
+        token: "",
+      },
+      // form error
+      errorNewPassword: false,
+      errorNewPasswordMessage: "",
+
+      // show password field
+      showPassword: false,
+
+      status: this.$tr("Resetting password"),
+      error: null,
+
+      // input rules
+      rules: {
+        required: (value) => (value && Boolean(value)) || "Required",
+      },
+    };
+  },
+  validations: {
+    newPassword: {
+      password: Validations.passwordValidation,
+      confirm_password: Validations.password2Validation,
+      email: Validations.requiredValidation,
+      token: Validations.requiredValidation,
+    },
+  },
+  methods: {
+    checkValidations() {
+      this.$v.newPassword.token.$model = this.token;
+      this.$v.newPassword.email.$model = this.email;
+
+      if (this.$v.newPassword.$invalid) {
+        this.$toasterNA("red", this.$tr('please_fill_all_fields_correctly'));
+        return false;
+      }
+
+      return true;
+    },
+
+    async confirmPasswordReset() {
+      if (this.checkValidations()) {
+        try {
+          this.isLoading = true;
+          const response = await this.$axios.post(
+            "reset-password",
+            this.newPassword
+          );
+          if (response.status === 202) {
+            const data = response.data.data;
+            this.$router.push("/");
+            // this.$toastr.s(this.$tr("password_reset"));
+			this.$toasterNA("green", this.$tr('password_reset'));
+
+          }
+          if (response.status === 200) {
+            // this.$toastr.e(this.$tr("login_failed"));
+          this.$toasterNA("red",this.$tr("login_failed"));
+
+            // this.$toastr.e(
+            //   this.$tr("account_in_warning_because_reseting_3_times_in_an_hour")
+            // );
+          this.$toasterNA("red",this.$tr("account_in_warning_because_reseting_3_times_in_an_hour"));
+
+            this.$router.push("/");
+          }
+        } catch (error) {
+          if (error.response.status === 400) {
+            // this.$toastr.e(this.$tr("token_expired"));
+          this.$toasterNA("red",this.$tr("token_expired"));
+
+          } else if (error.response.status == 422) {
+            // return server errors
+            const errors = error.response.data.errors;
+            for (let error in errors) {
+              for (let er in errors[error]) {
+                // this.$toastr.e(this.$tr(errors[error][er]));
+          this.$toasterNA("red",this.$tr(errors[error][er]));
+
+              }
+            }
+          } else {
+            // this.$toastr.e(this.$tr(error.response.statusText));
+          this.$toasterNA("red",this.$tr(error.response.statusText));
+
+          }
+        }
+        this.isLoading = false;
+      }
+    },
+    resetErrors() {
+      this.errorNewPassword = false;
+      this.errorNewPasswordMessage = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-sheet.v-card:not(.v-sheet--outlined) {
+  box-shadow: unset;
+}
+.theme--light.v-card {
+  background-color: rgba(232, 221, 221, 0.804);
+  color: rgba(0, 0, 0, 0.87);
+}
+#myVideo {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+</style>
