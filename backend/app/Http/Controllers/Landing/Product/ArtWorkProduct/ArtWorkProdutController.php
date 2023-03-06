ec5b9fbfdf67a8ffd8f4099c95a2c79b5c3d86c1
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-03-06 10:47:35 +0430

    dd

---
+++ backend/app/Http/Controllers/Landing/Product/ArtWorkProduct/ArtWorkProdutController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Landing\Product\ArtWorkProduct;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ArtWorkProdutController extends Controller
+{
+    //
+}
