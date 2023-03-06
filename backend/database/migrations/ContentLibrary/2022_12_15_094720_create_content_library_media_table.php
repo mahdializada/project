5de7bbcd252080f371c145b7edf67c3a551c620e
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-03-06 15:06:52 +0430

    dd

---
+++ backend/database/migrations/ContentLibrary/2022_12_15_094720_create_content_library_media_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\ContentLibrary\ContentLibraryMedia;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentLibraryMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content_library_media', function (Blueprint $table) {
+            $table->id();
+            $table->string('project_url');
+            $table->string('media_size');
+            $table->enum('status', ContentLibraryMedia::getTypes())->default('not_publish');
+            $table->foreignUuid('content_library_id')->constrained()->cascadeOnUpdate()->nullable()->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content_library_media');
+    }
+}
