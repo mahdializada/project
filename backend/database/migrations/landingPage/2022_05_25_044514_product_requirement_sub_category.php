d3b9adbd98f559300fda71883a8c1855a8cfbb9e
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-03-06 15:07:24 +0430

    ss

---
+++ backend/database/migrations/landingPage/2022_05_25_044514_product_requirement_sub_category.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Landing\Master\Product\Requirement\RequirementSubCategory;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductRequirementSubCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_requirement_sub_categories', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string("name");
+            $table->string("code")->unique();
+            $table->string("icon")->nullable();
+            $table->text("description")->nullable();
+            $table->enum("status", RequirementSubCategory::getTypes())->default("pending");
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignUuid('created_by')->nullable()->constrained("users")->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignUuid('category_id')->nullable()->constrained("product_requirement_categories")->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_requirement_sub_categories');
+    }
+}
