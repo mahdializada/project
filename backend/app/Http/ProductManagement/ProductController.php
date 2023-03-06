78b97e5113b5061491b5b89440dfe916ef06b60e
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-03-06 15:04:01 +0430

    ss

---
+++ backend/app/Http/ProductManagement/ProductController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\ProductManagement;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\ProductManagement\ProductRepository;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    private $repository;
+    public function __construct()
+    {
+        $this->repository = new ProductRepository();
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return $this->repository->paginate($request);
+    }
+
+    public function images(Request $request)
+    {
+        return $this->repository->images($request);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate($this->repository->storeRules());
+        return $this->repository->store($request);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+
+    public function checkSku(Request $request)
+    {
+        return $this->repository->checkSku($request);
+    }
+}
