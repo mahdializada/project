6a7f79c1e1a1a953fe2e9a14441653782ccc786c
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-03-06 15:03:19 +0430

    ff

---
+++ backend/app/Http/Landing/Product/Master/ProductSubCategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Landing\Product\Master;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Landing\RootController;
+use App\Models\Landing\Master\Product\ProductSubCategory;
+use App\Repositories\Landing\Product\Master\SubCategory\ProductRepository;
+
+
+class ProductSubCategoryController extends RootController
+{
+    protected $repository;
+
+    public function __construct()
+    {
+        $this->repository = new ProductRepository(new ProductSubCategory());
+    }
+
+    public function index(Request $request){
+        return $this->repository->paginate($request);
+    }
+
+    public function search(Request $request)
+    {
+        return $this->repository->search($request);
+    }
+
+    public function changeStatus(Request $request)
+    {
+        return  $this->repository->changeSubCategoriesStatus($request);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        return $this->repository->changedRemovedStatus($id);
+    }
+}
