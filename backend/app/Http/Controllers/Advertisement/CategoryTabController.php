5470c185f96f1174756eaa3c51aafb538944b840
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-03-06 10:46:15 +0430

    ss

---
+++ backend/app/Http/Controllers/Advertisement/CategoryTabController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Advertisement;
+
+use App\Http\Controllers\Controller;
+use App\Models\Advertisement\CategoryTab;
+use Illuminate\Http\Request;
+
+class ColumnTabController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function getTabCategory(Request $request)
+    {
+        $category_tab =  CategoryTab::with('category:id,category_name')->where('tab_name', $request->tabName);
+        return response()->json($category_tab);
+    }
+}
