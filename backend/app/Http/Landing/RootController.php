b075d1ee4019dd9d04286b114b1d90f40f0e2c57
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-03-06 15:03:01 +0430

    dd

---
+++ backend/app/Http/Landing/RootController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Landing;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\Landing\Product\RootRepository;
+use Illuminate\Http\Request;
+
+class RootController extends Controller
+{
+    
+}
