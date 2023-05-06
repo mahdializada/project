70137e9c5b96eb8eed6f7eb8cd18250b9a19fa5e
Mohammad Mahdi Alizada <68942156+mahdializada@users.noreply.github.com>
2023-05-06 16:10:12 +0430

    ee

--- frontend/pages/advertisement/ipa.vue
+++ frontend/pages/advertisement/ipa.vue
@@ -5,7 +5,6 @@
       <IPAFilter ref="ipaFilter" @filterRecords="onFilterRecords" />
       <StepperComponent
         ref="ipaComponentStepper"
-
         @pushRecord="pushRecord"
         @updateRecord="updateRecord"
       />
